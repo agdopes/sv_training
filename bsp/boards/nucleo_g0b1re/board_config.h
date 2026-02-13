d1b10ef2ca9399386ab8d6e3928dde8d1b6ef05e
gdopes <gdoping@siliconverified.com>
2026-02-13 14:58:50 +0800

    update Cmake files and folder structure

---
+++ bsp/boards/nucleo_g0b1re/board_config.h
@@ -0,0 +1,24 @@
+/**
+ * @file board_config.h
+ * @brief Board-specific configuration for STM32G0B1RE Nucleo
+ */
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+// // UART Configuration
+// #define BOARD_UART_INSTANCE     2       // USART2
+// #define BOARD_UART_BAUDRATE     115200
+// #define BOARD_UART_TX_PIN       2       // PA2
+// #define BOARD_UART_RX_PIN       3       // PA3
+
+// // Clock Configuration
+// #define BOARD_SYSCLK_HZ         64000000
+// #define BOARD_AHB_PRESCALER     1
+// #define BOARD_APB1_PRESCALER    1
+// #define BOARD_APB2_PRESCALER    1
+
+// // LED Configuration
+// #define BOARD_LED_PORT          0       // GPIOA
+// #define BOARD_LED_PIN           5
+
+#endif // BOARD_CONFIG_H
